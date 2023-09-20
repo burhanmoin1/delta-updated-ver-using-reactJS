f63f7308110538e562334d3f5e30497450c430bc
Burhan <m.burhanmoin1@gmail.com>
2023-09-20 05:00:33 -0700

    'security-update'

--- src/ThankYou.js
+++ src/ThankYou.js
@@ -8,7 +8,7 @@ const ThankYou = () => {
     <div className="thank-you-container">
       <h2 className="thank-you-heading">Thank You!</h2>
       <p className="thank-you-paragraph">
-        Your submission has been received. We'll get back to you shortly.
+        Your application has been received, one of our support agents will get in contact with you shortly. Thank you for registering with Delta Communication Network.
       </p>
     </div>
   );
